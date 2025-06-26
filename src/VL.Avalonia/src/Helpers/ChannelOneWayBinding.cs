ee3c195bb9a52b8131bf16e8be855222f1e572e7
antokhio <antokhio@gmail.com>
2025-06-26 20:37:10 +0300

    autocmpletebox wip

---
+++ src/VL.Avalonia/src/Helpers/ChannelOneWayBinding.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using VL.Lib.Reactive;
+
+namespace VL.Avalonia.Helpers;
+
+public class ChannelOneWayBinding<TProperty>
+{
+    private readonly AvaloniaProperty<TProperty?> _property;
+    private readonly AvaloniaObject _control;
+
+    public ChannelOneWayBinding(AvaloniaObject control, AvaloniaProperty<TProperty?> property)
+    {
+        _property = property;
+        _control = control;
+    }
+
+    private IChannel<TProperty>? _channel;
+    public void SetChannel(IChannel<TProperty>? channel)
+    {
+        if (_channel != channel)
+        {
+            if (channel != null)
+            {
+                _control.Bind(_property, channel);
+            }
+
+            _channel = channel;
+        }
+    }
+}
