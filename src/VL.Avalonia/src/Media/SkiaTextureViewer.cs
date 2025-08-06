29ab9a6f48d7504e4f4f7c03dabb23b33123688c
cloneproduction <clone@cloneproduction.net>
2025-08-06 23:54:40 +0800

    added TextureViewer and demo patch

---
+++ src/VL.Avalonia/src/Media/SkiaTextureViewer.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Media;
+using SkiaSharp;
+using VL.Avalonia.Attributes;
+using VL.Core;
+using VL.Core.Import;
+
+
+namespace VL.Avalonia.Controls.Media
+{
+    //[ProcessNode(HasStateOutput = true, Name = "TextureViewer", FragmentSelection = FragmentSelection.Explicit)]
+
+    /// <summary>
+    /// The <c>SkiaTextureViewer</c> control to see display an SKImage
+    /// </summary>
+    [ProcessNode(Name = "TextureViewer")]
+
+    public partial class SkiaTextureViewer : ControlWrapperBase<TextureViewer>
+    {
+        [ImplementProperty("TextureViewer.SourceProperty", Order = -10)]
+        protected Optional<SKImage> _source;
+
+        [ImplementProperty("TextureViewer.StretchProperty", Order = -10)]
+        protected Optional<Stretch> _stretch;
+
+        [ImplementProperty("TextureViewer.StretchDirectionProperty", Order = -10)]
+        protected Optional<StretchDirection> _stretchDirection;
+    }
+}
