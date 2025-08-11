5b08ef3b1324b4c8b2d97e3d75bafbd74bf2cb6d
antokhio <antokhio@gmail.com>
2025-08-11 19:20:37 +0300

    added ToSolidColorBrush, ToColor

--- src/VL.Avalonia/src/Extensions/MathExtensions.cs
+++ src/VL.Avalonia/src/Extensions/MathExtensions.cs
@@ -4,39 +4,65 @@ using Point = Avalonia.Point;
 
 namespace VL.Avalonia;
 
-public static class MathExtensions
+public static partial class Extensions
 {
+    /// <summary>
+    /// Converts Avalonia Point to Vector2
+    /// </summary>
     public static Vector2 FromPoint(this Point point) =>
-    new Vector2(
-        (float)point.X,
-        (float)point.Y
-    );
+        new Vector2((float)point.X, (float)point.Y);
 
+    /// <inheritdoc cref="FromPoint(Point)"/>
+    public static Vector2? FromPoint(Point? point) =>
+     point.HasValue ? point.Value.FromPoint() : null;
+
+    /// <summary>
+    /// Converts Vector2 to Avalonia.Point
+    /// </summary>
     public static Point ToPoint(this Vector2 point) =>
-        new Point(
-            point.X,
-            point.Y
-        );
+        new Point(point.X, point.Y);
 
+    /// <inheritdoc cref="ToPoint(Vector2)"/>
+    public static Point? ToPoint(Vector2? point) =>
+    point.HasValue ? point.Value.ToPoint() : null;
+
+    /// <summary>
+    /// Converts Avalonia.Rect to RectangleF
+    /// </summary>
     public static RectangleF FromRect(this Rect rect) =>
-        new RectangleF(
-            (float)rect.X,
-            (float)rect.Y,
-            (float)rect.Width,
-            (float)rect.Height
-        );
+        new RectangleF((float)rect.X, (float)rect.Y, (float)rect.Width, (float)rect.Height);
 
+    /// <inheritdoc cref="FromRect(Rect)"/>
+    public static RectangleF? FromRect(Rect? rect) =>
+    rect.HasValue ? rect.Value.FromRect() : null;
+
+    /// <summary>
+    /// Converts RectangleF to Avalonia.Rect
+    /// </summary>
     public static Rect ToRect(this RectangleF rect) =>
-        new Rect(
-            (double)rect.X,
-            (double)rect.Y,
-            (double)rect.Width,
-            (double)rect.Height
-        );
+           new Rect((double)rect.X, (double)rect.Y, (double)rect.Width, (double)rect.Height);
 
+    /// <inheritdoc cref="ToRect(RectangleF)"/>
+    public static Rect? ToRect(RectangleF? rect) =>
+    rect.HasValue ? rect.Value.ToRect() : null;
+
+    /// <summary>
+    /// Converts Avalonia.Vector to Vector2
+    /// </summary>
     public static Vector2 FromVector(this Vector vector) =>
         new Vector2((float)vector.X, (float)vector.Y);
 
+    /// <inheritdoc cref="FromVector(Vector)"/>
+    public static Vector2? FromVector(Vector? vector) =>
+    vector.HasValue ? vector.Value.FromVector() : null;
+
+    /// <summary>
+    /// Converts Vector2 to Avalonia.Vector
+    /// </summary>
     public static Vector ToVector(this Vector2 vector) =>
         new Vector((double)vector.X, (double)vector.Y);
+
+    /// <inheritdoc cref="ToVector(Vector2)"/>
+    public static Vector? ToVector(Vector2? vector) =>
+    vector.HasValue ? vector.Value.ToVector() : null;
 }
