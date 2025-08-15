efc486b2796a4e516dd772e14c8f13792905665b
antokhio <antokhio@gmail.com>
2025-08-15 14:41:07 +0300

    removed old data template, added DataTemplate, fixes on help patches

---
+++ src/VL.Avalonia/src/Controls/Base/IDataTemplateRegion.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+
+namespace VL.Avalonia.Controls.Base
+{
+    /// <summary>
+    /// Base interface for DataTemplate region in VL
+    /// You can check DataTemplate in vl definitions
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    /// <typeparam name="TIn"></typeparam>
+    public interface IDataTemplateRegion<TState, TIn>
+    {
+        /// <summary>
+        /// Used internally to convert patch to delegate
+        /// </summary>
+        public TState InvokeRegion(TState state, TIn input, out Control output);
+    }
+}
