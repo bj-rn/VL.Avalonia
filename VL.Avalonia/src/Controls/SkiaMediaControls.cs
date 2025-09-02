df767f9cafb39a01acf65e03b775f02797a301db
antokhio <antokhio@gmail.com>
2025-09-02 15:06:36 +0300

    refactored a bit both controls so they share codebase, renamed to SkiaImageControl, SkiaPictureControl

---
+++ VL.Avalonia/src/Controls/SkiaMediaControls.cs
@@ -0,0 +1,93 @@
+﻿using SkiaSharp;
+using VL.Avalonia.Skia;
+using VL.Core;
+using VL.Core.Import;
+using VL.Lib.Mathematics;
+namespace VL.Avalonia.Controls
+{
+    [ProcessNode]
+    public abstract class SkiaMediaControlWrapperBase<T> : ControlWrapperBase<T> where T : SkiaMediaControlBase, new()
+    {
+        protected Optional<SizeMode> _mode;
+        public void SetSizeMode(Optional<SizeMode> mode)
+        {
+            if (_mode != mode)
+            {
+                if (mode.HasValue)
+                {
+                    _output.Mode = mode.Value;
+                }
+                else
+                {
+                    _output.Mode = null;
+                }
+
+                _mode = mode;
+            }
+        }
+
+        protected Optional<RectangleAnchor> _anchor;
+        public void SetAnchor([Pin(Visibility = Model.PinVisibility.Optional)] Optional<RectangleAnchor> anchor)
+        {
+            if (_anchor != anchor)
+            {
+                if (anchor.HasValue)
+                {
+                    _output.Anchor = anchor.Value;
+                }
+                else
+                {
+                    _output.Anchor = null;
+                }
+
+                _anchor = anchor;
+            }
+        }
+    }
+
+    [ProcessNode(Name = "SkiaImageControl")]
+    public partial class SkiaImageControlWrapper : SkiaMediaControlWrapperBase<SkiaImageControl>
+    {
+        protected Optional<SKImage> _image;
+        [Fragment(Order = PinOrder.Main)]
+        public void SetImage(Optional<SKImage> image)
+        {
+            if (_image != image)
+            {
+                if (image.HasValue)
+                {
+                    _output.Image = image.Value;
+                }
+                else
+                {
+                    _output.Image = null;
+                }
+
+                _image = image;
+            }
+        }
+    }
+
+    [ProcessNode(Name = "SkiaPictireControl")]
+    public partial class SkiaPictureControlWrapper : SkiaMediaControlWrapperBase<SkiaPictureControl>
+    {
+        protected Optional<SKPicture> _picture;
+        [Fragment(Order = PinOrder.Main)]
+        public void SetPicture(Optional<SKPicture> picture)
+        {
+            if (_picture != picture)
+            {
+                if (picture.HasValue)
+                {
+                    _output.Picture = picture.Value;
+                }
+                else
+                {
+                    _output.Picture = null;
+                }
+
+                _picture = picture;
+            }
+        }
+    }
+}
---
+++ VL.Avalonia/src/Skia/SkiaMediaControlBase.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Threading;
+using SkiaSharp;
+using VL.Lib.Mathematics;
+
+namespace VL.Avalonia.Skia
+{
+    public abstract class SkiaMediaControlBase : Control
+    {
+        public SizeMode? Mode { get; set; }
+        public RectangleAnchor? Anchor { get; set; }
+        public abstract override void Render(DrawingContext context);
+    }
+
+    public class SkiaImageControl : SkiaMediaControlBase
+    {
+        public SKImage? Image { get; set; }
+        public override void Render(DrawingContext context)
+        {
+            context.Custom(new SkiaImageDrawingOperation(Image, Bounds, Mode ?? SizeMode.AutoWidth, Anchor ?? RectangleAnchor.Center));
+            Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Background);
+        }
+    }
+
+    public class SkiaPictureControl : SkiaMediaControlBase
+    {
+        public SKPicture? Picture { get; set; }
+        public override void Render(DrawingContext context)
+        {
+            context.Custom(new SkiaPictureDrawingOperation(Picture, Bounds, Mode ?? SizeMode.AutoWidth, Anchor ?? RectangleAnchor.Center));
+            Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Background);
+        }
+    }
+}
