ba7f562d0b5e25e3e15191064ef68d7dca8c25dd
antokhio <antokhio@gmail.com>
2025-10-06 20:57:18 +0300

    Adds IsPointerOver, IsFocused and others InputElement extesnisons, adds hit test to skia controls

--- VL.Avalonia/src/Controls/Extensions/InputElementExtensions.cs
+++ VL.Avalonia/src/Controls/Extensions/InputElementExtensions.cs
@@ -4,13 +4,31 @@ namespace VL.Avalonia.Controls;
 
 public static partial class Extensions
 {
-    /// <summary>
-    /// Focuses control inherited from InputElement
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="method"></param>
-    /// <param name="keyModifiers"></param>
-    /// <returns></returns>
-    public static void Focus(InputElement? input, NavigationMethod method = NavigationMethod.Unspecified, KeyModifiers keyModifiers = KeyModifiers.None) =>
-        input?.Focus(method, keyModifiers);
+    /// <summary>Focus InputElement (Control)</summary>
+    public static void Focus(InputElement? input, NavigationMethod method = NavigationMethod.Unspecified, KeyModifiers keyModifiers = KeyModifiers.None)
+        => input?.Focus(method, keyModifiers);
+
+    /// <inheritdoc cref="InputElement.Focusable" />
+    public static bool Focusable(InputElement? input)
+        => input?.IsEnabled ?? false;
+
+    /// <inheritdoc cref="InputElement.IsEnabled" />
+    public static bool IsEnabled(InputElement? input)
+        => input?.IsEnabled ?? false;
+
+    /// <inheritdoc cref="InputElement.Cursor" />
+    public static Cursor? Cursor(InputElement? input)
+        => input?.Cursor;
+
+    /// <inheritdoc cref="InputElement.IsKeyboardFocusWithin" />
+    public static bool IsKeyboardFocusWithin(InputElement? input)
+        => input?.IsKeyboardFocusWithin ?? false;
+
+    /// <inheritdoc cref="InputElement.IsFocused" />
+    public static bool IsFocused(InputElement? input)
+        => input?.IsFocused ?? false;
+
+    /// <inheritdoc cref="InputElement.IsPointerOver" />
+    public static bool IsPointerOver(InputElement? input)
+        => input?.IsPointerOver ?? false;
 }
