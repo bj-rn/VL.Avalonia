b6d087cdf3b23acb6e3ddb340c89dae649d41ca7
antokhio <antokhio@gmail.com>
2025-10-27 19:50:38 +0300

    feat: key gesture improvements

--- VL.Avalonia/src/KeyGesture.cs
+++ VL.Avalonia/src/KeyGesture.cs
@@ -5,20 +5,41 @@ using VL.Core.Import;
 
 namespace VL.Avalonia
 {
-    [ProcessNode(Name = "KeyGesture")]
-    public partial class KeyGestureWrapper
+    /// <summary>
+    /// Provides functionality for configuring key gesture input, including support for specifying
+    /// keyboard keys and modifiers.
+    /// </summary>
+    [ProcessNode]
+    public abstract class KeyGestureWrapperBase
     {
         protected KeyGesture _output = KeyGestureExtensions.Default();
         public KeyGesture Output => _output;
+    }
 
+    /// <inheritdoc cref="KeyGestureWrapperBase"/>
+    [ProcessNode(Name = "KeyGesture (String)")]
+    public partial class KeyGestureStringWrapper : KeyGestureWrapperBase
+    {
         private Optional<string> _gesture;
+
+        /// <param name="gesture">The key gesture string to parse. See <see cref="KeyGesture.Parse(string)"/> for format details.
+        /// <remarks>Ctrl + X</remarks>
+        /// </param>
         public void SetGesture(Optional<string> gesture)
         {
             if (_gesture != gesture)
             {
                 if (gesture.HasValue)
                 {
-                    _output = KeyGesture.Parse(gesture.Value);
+                    try
+                    {
+                        _output = KeyGesture.Parse(gesture.Value);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        _output = KeyGestureExtensions.Default();
+                    }
                 }
                 else
                 {
@@ -26,6 +47,27 @@ namespace VL.Avalonia
                 }
 
                 _gesture = gesture;
+            }
+        }
+    }
+
+    /// <inheritdoc cref="KeyGestureWrapperBase"/>
+    [ProcessNode(Name = "KeyGesture (Advanced)")]
+    public partial class KeyGestureWrapper : KeyGestureWrapperBase
+    {
+        private Key _key = Key.None;
+        private KeyModifiers _modifiers = KeyModifiers.None;
+
+        /// <param name="key">The keyboard key to use. See <see cref="Avalonia.Input.Key"/> for possible values</param>
+        /// <param name="modifiers">The keyboard modifiers to use. See <see cref="Avalonia.Input.Key"/> for possible values</param>
+        public void SetValue(Key key = Key.None, KeyModifiers modifiers = KeyModifiers.None)
+        {
+            if (_key != key || _modifiers != modifiers)
+            {
+                _output = new KeyGesture(key, modifiers);
+
+                _key = key;
+                _modifiers = modifiers;
             }
         }
     }
