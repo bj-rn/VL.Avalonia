65b400714d7e0b3e908c60451b6a6dd7a419e41f
antokhio <antokhio@gmail.com>
2026-04-13 15:16:26 +0300

    chore: removed unsused attributes

--- VL.Avalonia/src/Attributes/ImplementContent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VL.Avalonia.Attributes
-{
-    /// <summary>
-    /// Used in CodeGen, implements content as Optional `object`
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
-    internal sealed class ImplementContent : Attribute { }
-}
--- VL.Avalonia/src/Attributes/ImplementIsEnabled.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VL.Avalonia.Attributes
-{
-    /// <summary>
-    /// Used in CodeGem to implement IsEnabled
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
-    internal sealed class ImplementIsEnabled<T> : Attribute { }
-}
