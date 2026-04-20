ad440342ef33a14215b8a082627630ce24491966
antokhio <antokhio@gmail.com>
2026-04-20 21:26:34 +0300

    headered content control, expander, tab item

---
+++ VL.Avalonia/src/Controls/Base/Control/HeaderedControlNodeBase.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using VL.Avalonia.Attributes;
+using VL.Core;
+using VL.Core.Import;
+
+namespace VL.Avalonia.Controls
+{
+    /// <summary>
+    /// Base wrapper for <see cref="HeaderedContentControl"/>.
+    /// </summary>
+    [ProcessNode]
+    public abstract partial class HeaderedControlNodeBase<T> : ContentControlNodeBase<T>
+        where T : HeaderedContentControl, new()
+    {
+        /// <summary>Sets the header content (can be a string, UI element, or any object).</summary>
+        [ImplementProperty(
+            typeof(HeaderedContentControl),
+            nameof(HeaderedContentControl.HeaderProperty),
+            Order = PinOrder.Secondary,
+            PinVisibility = Model.PinVisibility.Optional
+        )]
+        private Optional<object> _header;
+
+        /// <summary>Sets the data template used to display the header content.</summary>
+        [ImplementProperty(
+            typeof(HeaderedContentControl),
+            nameof(HeaderedContentControl.HeaderTemplateProperty),
+            Order = PinOrder.DataTemplate,
+            PinVisibility = Model.PinVisibility.Optional
+        )]
+        private Optional<IDataTemplate> _headerTemplate;
+    }
+}
