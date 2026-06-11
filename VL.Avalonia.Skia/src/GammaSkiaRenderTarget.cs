2036ad8621ae9b7a47d895b513b6976e2cb228f7
Elias Holzer <elias@vvvv.org>
2026-06-11 20:32:18 +0200

    Use existing Skia surface from vvvv (if possible), do not create new surface every frame, set Skia colorspace explicitely on surface

--- VL.Avalonia.Skia/src/GammaSkiaRenderTarget.cs
+++ VL.Avalonia.Skia/src/GammaSkiaRenderTarget.cs
@@ -1,21 +1,53 @@
-﻿using System.Runtime.CompilerServices;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Platform;
 using Avalonia.Skia;
 using Avalonia.Skia.Helpers;
 using SkiaSharp;
+using System.Diagnostics;
+using System.Reactive.Concurrency;
+using System.Runtime.CompilerServices;
 using VL.Skia;
 
 namespace VL.Avalonia.Skia
 {
     sealed class GammaSkiaRenderTarget : IRenderTarget, ISkiaGpuRenderTarget
     {
+        private readonly Action<GammaSkiaRenderTarget> _onDispose;
         private readonly CallerInfo _callerInfo;
+        private readonly SKSurface _surface;
+        private readonly bool _ownsSurface;
 
-        public GammaSkiaRenderTarget(CallerInfo callerInfo)
+        public GammaSkiaRenderTarget(Action<GammaSkiaRenderTarget> onDispose, CallerInfo callerInfo)
         {
+            _onDispose = onDispose;
             _callerInfo = callerInfo;
+
+            // From vvvv 7.3 onwards CallerInfo includes a Surface property which we can use directly
+            var surfaceProperty = typeof(CallerInfo).GetProperty("Surface");
+            if (surfaceProperty != null && surfaceProperty.GetValue(callerInfo) is SKSurface surface)
+            {
+                _ownsSurface = false;
+                _surface = surface;
+            }
+            else
+            {
+                _ownsSurface = true;
+
+                var bounds = callerInfo.Canvas.DeviceClipBounds;
+                var width = Math.Max(1, bounds.Width);
+                var height = Math.Max(1, bounds.Height);
+                var info = new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+
+                // Try GPU surface first, fall back to raster
+                // IMPORTANT: Explicitly ask for TopLeft origin to match the SurfaceOrigin property
+                if (callerInfo.GRContext != null)
+                    _surface = SKSurface.Create(callerInfo.GRContext, false, info, 1, GRSurfaceOrigin.TopLeft);
+
+                _surface ??= SKSurface.Create(info);
+            }
         }
+
+        public CallerInfo CallerInfo => _callerInfo;
 
         public bool IsCorrupted
         {
@@ -30,7 +62,20 @@ namespace VL.Avalonia.Skia
 
         public ISkiaGpuRenderSession BeginRenderingSession()
         {
-            return new GammaSkiaGpuRenderSession(_callerInfo);
+            if (_ownsSurface)
+            {
+                return new GammaSkiaGpuRenderSession(_callerInfo, _surface);
+            }
+            else
+            {
+                return new NoOpSession()
+                {
+                    GrContext = _callerInfo.GRContext,
+                    SkSurface = _surface,
+                    ScaleFactor = _callerInfo.Scaling,
+                    SurfaceOrigin = GRSurfaceOrigin.TopLeft
+                };
+            }
         }
 
         public IDrawingContextImpl CreateDrawingContext(bool useScaledDrawing)
@@ -38,6 +83,28 @@ namespace VL.Avalonia.Skia
             return DrawingContextHelper.WrapSkiaCanvas(_callerInfo.Canvas, new Vector(96, 96));
         }
 
-        public void Dispose() { }
+        public void Dispose() 
+        {
+            if (_ownsSurface)
+            {
+                _surface.Dispose();
+            }
+            _onDispose.Invoke(this);
+        }
+
+        sealed class NoOpSession : ISkiaGpuRenderSession
+        {
+            public required GRContext GrContext { get; init; }
+
+            public required SKSurface SkSurface { get; init; }
+
+            public required double ScaleFactor { get; init; }
+
+            public required GRSurfaceOrigin SurfaceOrigin { get; init; }
+
+            public void Dispose()
+            {
+            }
+        }
     }
 }
